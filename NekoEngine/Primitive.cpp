b4d59f9dd20a0d4e0a03af1bd22efd49d16c6810
Sandra Alvarez <sandruskiag@gmail.com>
2018-09-19 17:07:56 +0200

    Glut removed

--- NekoEngine/Primitive.cpp
+++ NekoEngine/Primitive.cpp
@@ -2,9 +2,6 @@
 #include <gl/GL.h>
 #include <gl/GLU.h>
 #include "Primitive.h"
-#include "glut/glut.h"
-
-#pragma comment(lib, "glut/glut32.lib")
 
 Primitive::Primitive() : transform(IdentityMatrix), color(White), wire(false), axis(false), type(PrimitiveTypes::Primitive_Point)
 {}
@@ -162,7 +159,7 @@ Sphere::Sphere(float radius) : Primitive(), radius(radius)
 
 void Sphere::InnerRender() const
 {
-	glutSolidSphere(radius, 25, 25);
+	//glutSolidSphere(radius, 25, 25);
 }
 
 // CYLINDER ============================================

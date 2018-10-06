543294418ac27e4e05a89c30e9312166b610e657
DatBeQuiet <kayter72@gmail.com>
2018-10-06 17:28:35 +0200

    camera rotation speed reduced

--- Source/ModuleCamera3D.cpp
+++ Source/ModuleCamera3D.cpp
@@ -63,7 +63,7 @@ update_status ModuleCamera3D::Update(float dt)
 		int dx = -App->input->GetMouseXMotion();
 		int dy = -App->input->GetMouseYMotion();
 
-		float Sensitivity = 0.25f;
+		float Sensitivity = 0.01f;
 
 		Position -= Reference;
 

7a717b57c8936379a3c6a898db69b2aeeee39bfa
datbequiet <kayter72@gmail.com>
2018-10-15 13:56:45 +0200

    improved herarchy panel

--- Source/PanelHierarchy.cpp
+++ Source/PanelHierarchy.cpp
@@ -20,6 +20,16 @@ bool PanelHierarchy::Draw()
 	if (ImGui::Begin(name, &enabled, hierarchyFlags))
 	{
 		GameObject* root = App->scene->root;
+
+		if (ImGui::BeginPopupContextWindow())
+		{
+			if (ImGui::Selectable("Create object")) {
+				App->GOs->CreateGameObject("patata", root);
+				ImGui::CloseCurrentPopup();
+			}
+			ImGui::EndPopup();
+		}
+
 		if (ImGui::TreeNodeEx(root->GetName()))
 		{
 			IterateAllChildren(root);
@@ -48,6 +58,10 @@ void PanelHierarchy::IterateAllChildren(GameObject* root)
 					// TODO: Bug with contextitem and isitemhovered(). only workig if treenode is open.
 					if (ImGui::BeginPopupContextItem())
 					{
+						if (ImGui::Selectable("Create object")) {
+							App->GOs->CreateGameObject((char*)child->GetName(), child);
+							ImGui::CloseCurrentPopup();
+						}
 						if (ImGui::Selectable("Delete")) {
 							MessageBox(0, "CHECK WARNING TODO at Gos's PostUdate Method", "MessageBox caption", MB_OK);
 							ImGui::CloseCurrentPopup();
@@ -66,6 +80,18 @@ void PanelHierarchy::IterateAllChildren(GameObject* root)
 				treeNodeFlags |= ImGuiTreeNodeFlags_Leaf;
 				ImGui::TreeNodeEx(child->GetName(), treeNodeFlags);
 				ImGui::TreePop();
+				if (ImGui::BeginPopupContextItem())
+				{
+					if (ImGui::Selectable("Create object")) {
+						App->GOs->CreateGameObject("aaa", child);
+						ImGui::CloseCurrentPopup();
+					}
+					if (ImGui::Selectable("Delete")) {
+						MessageBox(0, "CHECK WARNING TODO at Gos's PostUdate Method", "MessageBox caption", MB_OK);
+						ImGui::CloseCurrentPopup();
+					}
+					ImGui::EndPopup();
+				}
 				if (ImGui::IsMouseClicked(0) && ImGui::IsItemHovered())
 					App->scene->currentGameObject = child;
 			}

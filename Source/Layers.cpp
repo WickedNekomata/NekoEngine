62829408b6b2fd3bfa938c39cc8feb988f111503
Sandra Alvarez <sandruskiag@gmail.com>
2019-02-04 15:50:12 +0100

    Default layers collision

--- Source/Layers.cpp
+++ Source/Layers.cpp
@@ -29,8 +29,15 @@ Layers::Layers()
 	layers.reserve(MAX_NUM_LAYERS);
 	layers.shrink_to_fit();
 
+	uint filterMask = 0;
 	for (uint i = 0; i < MAX_NUM_LAYERS; ++i)
+	{
 		layers.push_back(new Layer(i));
+		filterMask |= layers[i]->GetFilterGroup();
+	}
+
+	for (uint i = 0; i < MAX_NUM_LAYERS; ++i)
+		layers[i]->filterMask = filterMask;
 
 	layers[0]->name = "Default";
 	layers[0]->builtin = true;

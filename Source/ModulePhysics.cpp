d76d5e593da74abb2ddf2df897aea29cdf874778
Sandra Alvarez <sandruskiag@gmail.com>
2019-02-25 16:49:44 +0100

    Overlap Sphere

--- Source/ModulePhysics.cpp
+++ Source/ModulePhysics.cpp
@@ -1031,6 +1031,14 @@ bool ModulePhysics::Overlap(physx::PxGeometry& geometry, physx::PxTransform& tra
 	return status;
 }
 
+bool ModulePhysics::OverlapSphere(float radius, math::float3 center, std::vector<OverlapHit>& touchesInfo, uint filterMask, SceneQueryFlags sceneQueryFlags) const
+{
+	physx::PxSphereGeometry gSphereGeometry(radius);
+	physx::PxTransform gTransform(physx::PxVec3(center.x, center.y, center.z));
+	
+	return Overlap(gSphereGeometry, gTransform, touchesInfo, filterMask, sceneQueryFlags);;
+}
+
 // ----------------------------------------------------------------------------------------------------
 
 void ModulePhysics::SetGravity(math::float3& gravity)

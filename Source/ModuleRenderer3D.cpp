478cc22de4d2ec6fe34628cd23d02733e9b76226
Sandra Alvarez <sandruskiag@gmail.com>
2019-02-26 15:44:16 +0100

    Different matrix order

--- Source/ModuleRenderer3D.cpp
+++ Source/ModuleRenderer3D.cpp
@@ -986,16 +986,15 @@ void ModuleRenderer3D::DrawProjectedTexture(ComponentProjector* toDraw) const
 		0.0f, 0.0f, 0.5f, 0.5f,
 		0.0f, 0.0f, 0.0f, 1.0f
 	); // converts the view frustum to a range between 0 and 1 in x and y
-	bias_matrix.Transpose();
 
 	// Specific mesh uniforms
-	math::float4x4 projector_view_matrix = toDraw->GetOpenGLViewMatrix(); // view
-	math::float4x4 projector_proj_matrix = toDraw->GetOpenGLProjectionMatrix(); // projection
-	math::float4x4 projector_matrix = projector_view_matrix * projector_proj_matrix * bias_matrix; // into texture space																					   
+	math::float4x4 projector_view_matrix = toDraw->GetOpenGLViewMatrix().Transposed(); // view
+	math::float4x4 projector_proj_matrix = toDraw->GetOpenGLProjectionMatrix().Transposed(); // projection
+	math::float4x4 projector_matrix = bias_matrix * projector_proj_matrix *projector_view_matrix; // into texture space																					   
 
 	//http://www.opengl-tutorial.org/intermediate-tutorials/tutorial-16-shadow-mapping/
 	uint location = glGetUniformLocation(shaderProgram, "projector_matrix");
-	glUniformMatrix4fv(location, 1, GL_FALSE, projector_matrix.ptr());
+	glUniformMatrix4fv(location, 1, GL_TRUE, projector_matrix.ptr());
 
 	// Unknown uniforms
 	uint textureUnit = 0;

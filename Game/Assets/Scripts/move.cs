d95e5f35addd992f2f8c292bc8e77c48a664ff2f
David Valdivia <d.valdiviamartinez@gmail.com>
2019-02-27 23:09:53 +0100

    making movement but it doesn't work

---
+++ Game/Assets/Scripts/move.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using JellyBitEngine;
+
+public class move : JellyScript
+{
+    private RaycastHit hit;
+    private int mask = LayerMask.GetMask("Default");
+    private NavMeshAgent agent = null;
+
+    public override void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    //Called every frame
+    public override void Update()
+    {
+        if(agent == null)
+            agent = gameObject.GetComponent<NavMeshAgent>();
+
+        CheckForMouseClick();
+    }
+
+    private void CheckForMouseClick()
+    {
+        if (Input.GetMouseButton(MouseKeyCode.MOUSE_RIGHT))
+        {
+            Ray ray = Physics.ScreenToRay(Input.GetMousePosition(), Camera.main);
+            if (Physics.Raycast(ray, out hit, float.MaxValue, (uint)mask, SceneQueryFlags.Dynamic | SceneQueryFlags.Static))
+            {
+                if (agent != null)
+                {
+                    Debug.Log("GOING TO SPOT");
+                    agent.SetDestination(hit.point);
+                }
+                else
+                    Debug.Log("AGENT IS NULL");
+            }
+        }
+    }
+
+}

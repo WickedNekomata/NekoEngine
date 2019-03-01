759e53754cecb7c0b0e69a1bb8d1e8c76f585059
Aleix Gabarró <aleix.gab@gmail.com>
2019-03-01 11:59:59 +0100

    module freetype

---
+++ Source/ModuleFreetype.cpp
@@ -0,0 +1,13 @@
+#include "ModuleFreetype.h"
+
+
+ModuleFreetype::ModuleFreetype(bool start_enabled) : Module(start_enabled)
+{
+	//math::Frustum::ViewportToScreenSpace();
+}
+
+ModuleFreetype::~ModuleFreetype()
+{
+}
+
+

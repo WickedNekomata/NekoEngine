794dc197fd035c02d529afe6d6134be25e39da88
Julià Mauri Costa <julimacowork@gmail.com>
2019-03-02 01:12:59 +0100

    UIScriptTest with commen calls for UI scripting

---
+++ Game/Assets/Scripts/UIScriptTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using JellyBitEngine;
+using JellyBitEngine.UI;
+
+public class UIScriptTest : JellyScript
+{
+    private Button btn;
+    private RectTransform rect;
+
+    //Use this method for initialization
+    public override void Awake()
+    {
+        rect = gameObject.GetComponent<RectTransform>();
+        btn = gameObject.GetComponent<Button>();
+    }
+
+    //Called every frame
+    public override void Update()
+    {
+        if (UI.UIHovered())
+        {
+            switch (btn.state)
+            {
+                case ButtonState.IDLE:
+                    break;
+                case ButtonState.HOVERED:
+                    break;
+                case ButtonState.R_CLICK:
+                    break;
+                case ButtonState.L_CLICK:
+                    break;
+            }
+        }
+
+        //RectTransform - Set and Get
+        //rect.x
+        //rect.y
+        //rect.x_dist
+        //rect.y_dist
+    }
+
+    //Set trhough on inspector on component buton, like unity (give gameobject where is the script and select script and method)
+    public void OnClick()
+    {
+        Debug.Log("GG");
+    }
+}
+

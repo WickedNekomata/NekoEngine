cedec8aa174d423d3993a3f5d248350eb1fadd2d
Sandra Alvarez <sandruskiag@gmail.com>
2019-03-03 12:45:54 +0100

    Break chest

--- Source/ModulePhysics.cpp
+++ Source/ModulePhysics.cpp
@@ -242,7 +242,7 @@ update_status ModulePhysics::FixedUpdate()
 #endif
 
 	//Debug();
-	//DestroyChest();
+	DestroyChest();
 
 	return UPDATE_CONTINUE;
 }
@@ -474,7 +474,7 @@ void ModulePhysics::DestroyChest()
 				std::vector<Resource*> prefabs = App->res->GetResourcesByType(ResourceTypes::PrefabResource);
 				for (uint i = 0; i < prefabs.size(); ++i)
 				{
-					if (strcmp(prefabs[i]->GetName(), "BC1.pfb") == 0)
+					if (strcmp(prefabs[i]->GetName(), "BrokenChestFinal.pfb") == 0)
 					{
 						prefab = (ResourcePrefab*)prefabs[i];
 						break;
